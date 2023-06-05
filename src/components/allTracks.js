a37f141e5401b5bf959694497ea129985ae126f8
Alice Lo <115280336+siuusunn@users.noreply.github.com>
2023-06-05 21:48:37 +0200

    Showing artists names correctly

---
+++ src/components/allTracks.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+import { API } from '../lib/api';
+
+export default function AllTracks() {
+  const [allTracks, setAllTracks] = useState([]);
+
+  useEffect(() => {
+    API.GET(API.ENDPOINTS.allTracks).then(({ data }) => setAllTracks(data));
+    console.log(allTracks);
+  }, []);
+
+  return (
+    <div>
+      <h1>ALL TRACKS</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Artists</th>
+            <th>Duration</th>
+            <th>ISRC</th>
+          </tr>
+        </thead>
+        <tbody>
+          {allTracks.map((track) => (
+            <tr key={track.id}>
+              <td>{track.title}</td>
+              <td>{track.artist.map((artist) => artist.name).join(', ')}</td>
+              <td>{track.duration}</td>
+              <td>{track.isrc}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
