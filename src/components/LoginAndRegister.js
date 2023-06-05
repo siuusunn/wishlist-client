e087c2b4fe51d38c97ef4dc4dbb24b084b92039c
Alice Lo <115280336+siuusunn@users.noreply.github.com>
2023-06-05 22:39:25 +0200

    LoginAndRegister basic styling done

--- src/components/LoginAndRegister.js
+++ src/components/LoginAndRegister.js
@@ -1,11 +1,12 @@
 import Login from './Login';
 import Register from './Register';
+import '../styles/loginAndRegister.scss';
 
 export default function LoginAndRegister() {
   return (
-    <>
+    <div className='LoginAndRegisterContainer'>
       <Login />
       <Register />
-    </>
+    </div>
   );
 }
